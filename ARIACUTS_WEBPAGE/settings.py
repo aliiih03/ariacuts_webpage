bfca78e9e7dfc112193d23e60af6c2bf86478d54
aliiih03 <aliheidary822@gmail.com>
2025-09-11 23:43:36 +0330

    media root changed

--- ARIACUTS_WEBPAGE/settings.py
+++ ARIACUTS_WEBPAGE/settings.py
@@ -115,8 +115,9 @@ import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+# تنظیمات مدیا
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # import os رو فراموش نکن
+MEDIA_ROOT = '/media'  # مسیر دیسک لیارا که ساختی
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
