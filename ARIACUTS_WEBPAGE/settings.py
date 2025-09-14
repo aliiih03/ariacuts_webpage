231ab09a184f692fab771a68de4e7e87546ecf8f
aliiih03 <aliheidary822@gmail.com>
2025-09-15 00:11:45 +0330

    edit admin panel

--- ARIACUTS_WEBPAGE/settings.py
+++ ARIACUTS_WEBPAGE/settings.py
@@ -32,7 +32,9 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'grappelli',
+
+    'django_adminlte',
+    'django_adminlte_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
